cc4e427db1ffa3330cbf1a14c1b1f6d23a72c65b
Mathew Heard <mat999@gmail.com>
2018-09-25 14:48:15 +1000

    ...

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function _forEach(cb){
 LightEnum.prototype.get = function(i){
     let v = this._forwards[i]
     if(v !== undefined){
-        return {key: i, value: this.extractorFn(v)}
+        return {key: i, value: this._extractorFn(v)}
     }
     v = this._backwards[i]
     if(v !== undefined){
