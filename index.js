92c7b84fd1dd1fe942aa98a73bc4f0ea0ef7290a
Mathew Heard <splitice@users.noreply.github.com>
2018-10-03 14:50:18 +1000

    optimize sequential numeric value enums

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ function LightEnum(members, extractorFn, indexReverse = true){
     this._extractorFn = extractorFn
     this._backwards = {}
     if(indexReverse){
+        let isNumeric = true
         for(var i in members){
-            this._backwards[extractorFn(members[i])] = i
+            const key = extractorFn(members[i])
+            isNumeric &= !Number.isNaN(key)
+            this._backwards[key] = i
+        }
+        if(isNumeric){
+            const keys = Object.keys(this._backwards)
+            if(Math.max(...keys) == (keys.length - 1) && Math.min(...keys) == 0){
+                const _backwards = this._backwards
+                this._backwards = new Array(keys.length)
+                for(var i in _backwards){
+                    this._backwards[i] = _backwards[i]
+                }
+            }
         }
     }
     this.enums = {
